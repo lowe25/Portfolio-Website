24dd95222f29ccd63b051814b6961805fa5f94f2
Louie Cabral <louiealdrin25@gmail.com>
2021-10-14 16:21:21 +0800

    MediaQueries Added

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -9,19 +9,19 @@ const Project = () => {
             <div className="btn-div">
               <a
                 href="https://github.com/lowe25/Emman-CCTV-template"
-                target="_blank"
+                target="_blank" rel="noreferrer"
               >
                 <button className="btn-projDemo">Github</button>
               </a>
               <a
                 href="https://compassionate-brown-1c4cd0.netlify.app/"
-                target="_blank"
+                target="_blank" rel="noreferrer"
               >
                 <button className="btn-projDemo">Demo</button>
               </a>
               <a
                 href="https://gist.github.com/lowe25/9466bb60f387331831511f262770a1a2"
-                target="_blank"
+                target="_blank" rel="noreferrer"
               >
                 <button className="btn-projDemo">Code</button>{" "}
               </a>
@@ -29,19 +29,19 @@ const Project = () => {
             <h1>Written In:</h1>
             <div class="technologies">
               <section className="tech">
-                <img src="./img/html.png" className="prog" alt="HTML5 Image" />
+                <img src="./img/html.png" className="prog" alt="HTML5 Image1" />
               </section>
               <section className="tech">
-                <img src="./img/css.png" className="prog" alt="CSS3 Image" />
+                <img src="./img/css.png" className="prog" alt="CSS3 Image1" />
               </section>
               <section className="tech">
-                <img src="./img/sass.png" className="prog" alt="SASS Image" />
+                <img src="./img/sass.png" className="prog" alt="SASS Image1" />
               </section>
               <section className="tech">
                 <img
                   src="./img/javascript.png"
                   class="prog"
-                  alt="JavaScript Image"
+                  alt="JavaScript Image1"
                 />
               </section>
             </div>
@@ -52,19 +52,19 @@ const Project = () => {
             <div className="btn-div">
               <a
                 href="https://github.com/lowe25/Ecommerce-Design"
-                target="_blank"
+                target="_blank" rel="noreferrer"
               >
                 <button className="btn-projDemo">Github</button>
               </a>
               <a
                 href="https://youthful-murdock-a8b8bb.netlify.app/"
-                target="_blank"
+                target="_blank" rel="noreferrer"
               >
                 <button className="btn-projDemo">Demo</button>
               </a>
               <a
                 href="https://gist.github.com/lowe25/68bc7e0693db9e908999450709965471"
-                target="_blank"
+                target="_blank" rel="noreferrer"
               >
                 <button className="btn-projDemo">Code</button>
               </a>
@@ -72,19 +72,19 @@ const Project = () => {
             <h1>Written In:</h1>
             <div className="technologies">
               <section className="tech">
-                <img src="./img/html.png" className="prog" alt="HTML5 Image" />
+                <img src="./img/html.png" className="prog" alt="HTML5Image" />
               </section>
               <section className="tech">
-                <img src="./img/css.png" className="prog" alt="CSS3 Image" />
+                <img src="./img/css.png" className="prog" alt="CSS3Image" />
               </section>
               <section className="tech">
-                <img src="./img/sass.png" className="prog" alt="SASS Image" />
+                <img src="./img/sass.png" className="prog" alt="SASSImage" />
               </section>
               <section className="tech">
                 <img
                   src="./img/javascript.png"
                   className="prog"
-                  alt="JavaScript Image"
+                  alt="JavaScriptImage"
                 />
               </section>
             </div>
@@ -95,16 +95,16 @@ const Project = () => {
             <div className="btn-div">
               <a
                 href="https://github.com/lowe25/Weather-Website"
-                target="_blank"
+                target="_blank"  rel="noreferrer"
               >
                 <button className="btn-projDemo">Github</button>
               </a>
-              <a href="#" target="_blank">
+              <a href="#" target="_blank" rel="noreferrer">
                 <button className="btn-projDemo">Demo</button>
               </a>
               <a
                 href="https://gist.github.com/lowe25/4cf1fc0e52a6673b928c72a1d86559f4"
-                target="_blank"
+                target="_blank"  rel="noreferrer"
               >
                 {" "}
                 <button className="btn-projDemo">Code</button>
@@ -113,16 +113,16 @@ const Project = () => {
             <h1>Written In:</h1>
             <div className="technologies">
               <section className="tech">
-                <img src="./img/html.png" className="prog" alt="HTML5 Image" />
+                <img src="./img/html.png" className="prog" alt="HTML5Image" />
               </section>
               <section className="tech">
-                <img src="./img/css.png" className="prog" alt="CSS3 Image" />
+                <img src="./img/css.png" className="prog" alt="CSS3Image" />
               </section>
               <section className="tech">
                 <img
                   src="./img/javascript.png"
                   className="prog"
-                  alt="JavaScript Image"
+                  alt="JavaScriptImage"
                 />
               </section>
             </div>
@@ -133,13 +133,13 @@ const Project = () => {
             <div class="btn-div">
               <a
                 href="https://github.com/lowe25/Currency-Converter"
-                target="_blank"
+                target="_blank"  rel="noreferrer"
               >
                 <button class="btn-projDemo">Github</button>
               </a>
               <a
                 href="https://quizzical-dubinsky-86f204.netlify.app/"
-                target="_blank"
+                target="_blank"  rel="noreferrer"
               >
                 <button class="btn-projDemo">Demo</button>
               </a>
@@ -150,19 +150,19 @@ const Project = () => {
             <h1>Written In:</h1>
             <div class="technologies">
               <section class="tech">
-                <img src="./img/html.png" class="prog" alt="HTML5 Image" />
+                <img src="./img/html.png" class="prog" alt="HTML5Image" />
               </section>
               <section class="tech">
-                <img src="./img/css.png" class="prog" alt="CSS3 Image" />
+                <img src="./img/css.png" class="prog" alt="CSS3Image" />
               </section>
               <section class="tech">
-                <img src="./img/sass.png" class="prog" alt="SASS Image" />
+                <img src="./img/sass.png" class="prog" alt="SASSImage" />
               </section>
               <section class="tech">
                 <img
                   src="./img/javascript.png"
                   class="prog"
-                  alt="JavaScript Image"
+                  alt="JavaScriptImage"
                 />
               </section>
             </div>
@@ -173,13 +173,13 @@ const Project = () => {
             <div class="btn-div">
               <a
                 href="https://github.com/lowe25/Covid-19-Tracker"
-                target="_blank"
+                target="_blank"  rel="noreferrer"
               >
                 <button class="btn-projDemo">Github</button>
               </a>
               <a
                 href="https://heuristic-brown-19dc74.netlify.app/"
-                target="_blank"
+                target="_blank"  rel="noreferrer"
               >
                 <button class="btn-projDemo">Demo</button>
               </a>
@@ -190,19 +190,19 @@ const Project = () => {
             <h1>Written In:</h1>
             <div class="technologies">
               <section class="tech">
-                <img src="./img/html.png" class="prog" alt="HTML5 Image" />
+                <img src="./img/html.png" class="prog" alt="HTML5Image" />
               </section>
               <section class="tech">
-                <img src="./img/css.png" class="prog" alt="CSS3 Image" />
+                <img src="./img/css.png" class="prog" alt="CSS3Image" />
               </section>
               <section class="tech">
-                <img src="./img/sass.png" class="prog" alt="SASS Image" />
+                <img src="./img/sass.png" class="prog" alt="SASSImage" />
               </section>
               <section class="tech">
                 <img
                   src="./img/javascript.png"
                   class="prog"
-                  alt="JavaScript Image"
+                  alt="JavaScriptImage"
                 />
               </section>
             </div>
