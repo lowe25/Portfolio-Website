6b547f1041be1e6f6f6011a2885dc735d8e6449a
Louie Cabral <louiealdrin25@gmail.com>
2021-10-14 17:08:39 +0800

    Updated Navbar links hover design

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,45 @@
 const Navbar = () => {
+  const toggleBtn = () => {
+    const toggleButton = document.getElementsByClassName("toggle")[0];
+    const navbarLinks = document.getElementsByClassName("nav-links")[0];
+     toggleButton.addEventListener("click", () => {
+      navbarLinks.classList.toggle("active");
+    });
+  };
   return (
     <div>
-       <nav className="navbar">
-                <a href="#" className="company-name"></a>
-                <a href="#" className="toggle">
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                </a>
-                <div className="nav-links">
-                    <ul>
-                        <li><a href="#" className="links">Home</a></li>
-                        <li><a href="#about" className="links">About</a></li>
-                        <li><a href="#projects" className="links">Projects</a></li>
-                        <li><a href="#contact" className="links">Contact</a></li>
-                    </ul>
-                </div>
-            </nav>
+      <nav className="navbar">
+        <a href="#" className="company-name"></a>
+        <a href="#" className="toggle" onClick={toggleBtn}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </a>
+        <div className="nav-links">
+          <ul>
+            <li>
+              <a href="#" className="links">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="links">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#projects" className="links">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="links">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
