48f00cc109815f22e0e9cd69de0dd3b8d6941fd2
Louie Cabral <louiealdrin25@gmail.com>
2022-03-01 17:22:02 +0800

    Email Form Functionality Done

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,10 +1,47 @@
+import emailjs from "emailjs-com";
+import { useState } from "react";
 function Contacts() {
+  const [modal, setModal] = useState(true);
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+  function sendEmail(e) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_qcftgxw",
+        "template_7afjnwg",
+        e.target,
+        "RYE28SXdF61Szx4Ni"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
   return (
     <div className="contact-container">
       <p>
         Get in touch with me by filling up the form below or contacting me in my
         socials
       </p>
+      {modal && (
+        <div className="modal">
+          <div
+          onClick={toggleModal} 
+          className="overlay"></div>
+          <div className="modal-content">
+          <h1>Email Succesfully Sent</h1>
+          <button className="close-modal"
+          onClick={toggleModal}>X</button>
+          </div>
+        </div>
+      )}
       <div className="contact-details">
         <div className="contact">
           <ul>
@@ -15,25 +52,50 @@ function Contacts() {
               <i className="fas fa-phone-alt fa-2x"></i>09064935258
             </li>
             <li>
-              <i className="fas fa-envelope-open fa-2x"></i>louiealdrin25@gmail.com
+              <i className="fas fa-envelope-open fa-2x"></i>
+              louiealdrin25@gmail.com
             </li>
           </ul>
         </div>
         <div className="form">
-          <form className="contact-form">
+          <form onSubmit={sendEmail} className="contact-form">
             <label>Email</label>
-            <input type="text" placeholder="Email" className="txt-email" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="txt-email"
+              name="email"
+            />
+
+            <label>Name</label>
+            <input
+              type="text"
+              placeholder="Name"
+              className="txt-email"
+              name="name"
+            />
 
             <label>Subject</label>
-            <input type="text" placeholder="Subject" className="txt-email" />
+            <input
+              type="text"
+              placeholder="Subject"
+              className="txt-email"
+              name="subject"
+            />
 
             <label>Message</label>
             <textarea
               placeholder="Enter Message"
               className="txt-area"
+              name="message"
             ></textarea>
             <label></label>
-            <input type="button" value="Submit" className="btn-submit" />
+            <input
+              type="submit"
+              value="Submit"
+              className="btn-submit"
+              onClick={toggleModal}
+            />
           </form>
         </div>
       </div>
