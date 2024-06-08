cf2d666844a672e88f40ebc69d6be8e4678a4942
lowe25 <louiealdrin25@gmail.com>
2024-06-08 14:37:28 +0800

    Remove contact component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Footer from "./Components/Footer";
 import Landing from "./Pages/Landing";
 import About from "./Pages/About";
 import Projects from "./Pages/Projects";
-import Contacts from "./Pages/Contacts";
 function App() {
   return (
     <div>
@@ -20,10 +19,6 @@ function App() {
 
         <div id="projects">
           <Projects />
-        </div>
-
-        <div id="contacts">
-          <Contacts />
         </div>
         <Footer />
       </div>
